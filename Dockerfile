cb5efce381f30031f3215fa1761fd2bff87e3a0c
Nadeem Mohammad <coolmind182006@gmail.com>
2020-02-08 23:43:58 +0530

    Docker

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:10-15-1-jessie
+
+EXPOSE 5000
+
+ENV APP_ROOT=/root/app-root \
+    NODEJS_VERSION=8 \
+    NPM_RUN=start \
+    NAME=nodejs
+
+ENV HOME=${APP_ROOT}} \
+    NPM_CONFIG_PREFIX=${APP_ROOT}/.npm 
+
+
+COPY . ${APP_ROOT}
+
+RUN mkdir -p ${APP_ROOT} && \
+    mkdir -p ${APP_ROOT}/.npm && \
+    chown -R 1001:0 $${APP_ROOT} && \
+    chmod -R ug+rwx ${APP_ROOT}
+
+USER 1001
+
+WORKDIR ${APP_ROOT}
+
+RUN npm install && npm run build
+
+CMD node app_server.js
---
+++ app_server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middlware');
+
+const app = express();
+
+const apiService = process.env.API_ROUTE || 'api';
+const apiUrl = apiService ? `http://${apiService}` : 'http://api';
+
+console.log(apiUrl);
+
+var apiProxy = proxy('/api', {
+    target : apiUrl,
+    changeOrigin : false
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(apiProxy);
+
+app.get('/status', function(req, res) {
+    res.sendStatus(200);
+    console.log('working!');
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+let server = app.listen(5000);
